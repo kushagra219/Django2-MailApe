d4e2f4af7fbda212c37a49ce945707113436b911
Kushagra Garg <kushagra.garg1291@gmail.com>
2020-03-03 00:00:07 +0530

    modelsML

---
+++ mailinglist/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+import uuid
+
+from django.conf import settings
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+
+class MailingList(models.Model):
+    id = models.UUIDField(primary_key=True, 
+    default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=140)
+    owner = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse(
+            'mailinglist:manage_mailinglist',
+            kwargs={'pk':self.id}
+        )
+
+    def user_can_use_mailing_list(self, user):
+        return user == self.owner
+
+class Subscriber(models.Model):
+    id = models.UUIDField(primary_key=True, 
+    default=uuid.uuid4, editable=False)
+    email = models.EmailField()
+    confirmed = models.BooleanField(default=False)
+    mailing_list = models.ForeignKey(to=MailingList,
+    on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['email', 'mailing_list',]
